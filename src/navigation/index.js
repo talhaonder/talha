1ccc8fa9e88e530530357b0b30547f635f67edc0
talhaonder <ltalhaonderl@gmail.com>
2023-12-19 16:41:45 +0300

    screens

---
+++ src/navigation/index.js
@@ -0,0 +1,30 @@
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen'
+import WelcomeScreen from '../screens/WelcomeScreen'
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='Welcome'>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigation;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="flex-1 flex justify-around bg-white"> 
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  )
+}
